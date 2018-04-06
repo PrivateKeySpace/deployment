d38967d3ebe2fd9a32007023b23be1bed6176ac6
Valentyn Barmashyn <barmashyn.val@gmail.com>
2018-04-06 13:34:46 +0300

    PKSP-43 run binscript fixes

--- binscripts/run.sh
+++ binscripts/run.sh
@@ -5,56 +5,52 @@ set -e
 
 # logging functions
 log() { printf "%b\n" "$*"; }
-debug() { [[ ${PKS_DEBUG} != "true" ]] || printf "%b\n" "$*" >&2; }
+debug() { [[ ${PKS_DEBUG} != "1" ]] || printf "%b\n" "$*" >&2; }
 fail() { log "\nERROR: $*\n" >&2 ; exit 1 ; }
 
 pks_check_dependencies()
 {
   debug "\nChecking dependencies...\n"
 
-  which git >/dev/null 2>&1 || fail "Could not find 'git' command, make sure it's available first before continuing installation."
+  which curl >/dev/null 2>&1 || fail "Could not find 'curl' command, make sure it's available first before continuing installation."
   which docker >/dev/null 2>&1 || fail "Could not find 'docker' command, make sure it's available first before continuing installation."
   which docker-compose >/dev/null 2>&1 || fail "Could not find 'docker-compose' command, make sure it's available first before continuing installation."
 }
 
 pks_parse_params()
 {
-  PKS_FLAVOR=${1:-demo}
-  PKS_BRANCH=${2:-develop}
-  PKS_DIR=${3:-pks-deployment}
+  PKS_VERSION=${1:-"develop"}
+  PKS_FLAVOR=${2:-"regtest"}
+  PKS_DIR=${3:-"~/.pkswallet/config"}
 
+  debug "Version: '$PKS_VERSION'"
   debug "Flavor: '$PKS_FLAVOR'"
-  debug "Branch: '$PKS_BRANCH'"
   debug "Directory: '$PKS_DIR'"
 }
 
-pks_download_repo()
+pks_download_config()
 {
-  if [ ! -d "$PKS_DIR" ]; then
-    log "\nCloning deployment scripts into '$PKS_DIR'...\n"
+  log "\nDownloading config into '$PKS_DIR'...\n"
 
-    git clone https://github.com/PrivateKeySpace/deployment $PKS_DIR
-  else
-    log "\nFound '$PKS_DIR', proceeding..."
+  if [ ! -d "$PKS_DIR" ]; then
+    mkdir -p "$PKS_DIR"
   fi
+
+  curl -sSL "https://github.com/PrivateKeySpace/deployment/raw/$PKS_VERSION/config/docker-compose.$PKS_FLAVOR.yml" > "$PKS_DIR/docker-compose.$PKS_FLAVOR.yml"
 }
 
-pks_build_images()
+pks_pull_images()
 {
-  log "\nBuilding '$PKS_FLAVOR' Docker images for branch '$PKS_BRANCH'...\n"
+  log "\nPulling '$PKS_VERSION-$PKS_FLAVOR' images from Docker Hub...\n"
 
-  cd $PKS_DIR
-  FLAVOR=$PKS_FLAVOR BRANCH=$PKS_BRANCH make build
-  cd -
+  docker-compose --file "$PKS_DIR/docker-compose.$PKS_FLAVOR.yml" pull
 }
 
 pks_run_containers ()
 {
-  log "\nRunning '$PKS_FLAVOR' Docker images...\n"
+  log "\nRunning '$PKS_VERSION-$PKS_FLAVOR' containers...\n"
 
-  cd $PKS_DIR
-  FLAVOR=$PKS_FLAVOR make run
-  cd -
+  docker-compose --file "$PKS_DIR/docker-compose.$PKS_FLAVOR.yml" up
 }
 
 # entry point function
@@ -62,8 +58,8 @@ pks_run()
 {
   pks_check_dependencies
   pks_parse_params "$@"
-  pks_download_repo
-  pks_build_images
+  pks_download_config
+  pks_pull_images
   pks_run_containers
 }
 
